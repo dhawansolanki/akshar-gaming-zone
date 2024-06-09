aa81f86009dfe1d29515c67f30379bdd32ac3b1f
Dhawan Solanki <dhawansolanki2103@gmail.com>
2024-06-09 16:23:23 +0530

    fixed member and visitor

--- src/app/member/page.tsx
+++ src/app/member/page.tsx
@@ -2,14 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <div className="min-w-screen min-h-screen bg-white flex items-center justify-center px-5 py-5">
-        <div className="bg-white text-gray-800 rounded-xl shadow-lg overflow-hidden relative flex flex-col w-[414px] h-[736px]">
-          <div className="bg-white h-full w-full px-5 pt-6 pb-20 overflow-y-auto">
-            <div className="mb-5">
-              <img src="/home1.png" className="w-full" />
+    <div className="min-w-screen min-h-screen bg-white flex items-center flex-col justify-between">
+    <div className="bg-white text-gray-800 overflow-hidden relative flex-grow">
+      <div className="bg-white w-full overflow-y-auto">
+            <div className="mb-5" style={{"background":"url(/member2.png)", "backgroundRepeat":"no-repeat", backgroundPosition:"right", backgroundPositionY:"0%"}}>
+              <img src="/member1.png" className="w-100" />
             </div>
-            <form className="space-y-4">
+            <form className="space-y-4 px-5 pt-6 pb-20" >
               <div>
                 <label className="block text-orange-600">Phone No.</label>
                 <input type="text" placeholder="Enter your mobile number" className="w-full border-b-2 border-gray-300 focus:outline-none focus:border-orange-600" />
@@ -51,14 +50,14 @@ export default function Home() {
                 <button type="button" className="w-full bg-orange-600 text-white py-2 rounded-full mt-4">+ Add Member</button>
               </div>
               <div className="mt-4">
+                <div className="flex items-center mt-2">
+                  <input type="checkbox" className="form-checkbox text-orange-600" />
+                </div>
                 <p className="text-gray-500 text-sm">
                   I affirm that I have read and understood the rules of the game zone and agree to abide by them.
                   I acknowledge the inherent risks involved in participating in activities within the game zone and release the management from any liability.
                   My health is good, and I agree to follow staff instructions.
                 </p>
-                <div className="flex items-center mt-2">
-                  <input type="checkbox" className="form-checkbox text-orange-600" />
-                </div>
               </div>
               <div>
                 <button type="submit" className="w-full bg-orange-600 text-white py-3 rounded-full mt-4">Proceed to Payment</button>
@@ -67,6 +66,5 @@ export default function Home() {
           </div>
         </div>
       </div>
-    </>
   );
 }
