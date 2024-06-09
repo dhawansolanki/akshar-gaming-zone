09daf53bf3ded13b4df3e098438b80309d04ed75
Dhawan Solanki <dhawansolanki2103@gmail.com>
2024-06-09 15:57:05 +0530

    fixed home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,47 +11,50 @@ export default function Home() {
             </div>
           </div>
           <div className="text-center mb-12">
-            <p className="text-sm text-[#97D1CF] uppercase font-bold">Welcome to</p>
-            <h1 className="text-3xl text-[#EF4823] font-bold">Akshar Game Zone</h1>
+            <p className="text-sm text-[#97D1CF] uppercase font-bold">
+              Welcome to
+            </p>
+            <h1 className="text-3xl text-[#EF4823] font-bold">
+              Akshar Game Zone
+            </h1>
           </div>
         </div>
-        <div className="bg-white w-full flex">
-          <a
-            href="/member"
-            className="flex-1 flex items-center justify-center p-2 text-indigo-500 hover:text-indigo-500"
-          >
-            <div className="text-center">
-              <div className="rounded-full bg-[#FCB52D] text-black text-xs py-1 pl-2 pr-3 leading-none">
-                <i className="mdi mdi-fire text-base align-middle"></i>{" "}
-                <span className="align-middle">Member Form</span>
+        <div className="bg-white w-full flex items-start py-20">
+          <div className="w-full flex">
+            <a
+              href="/member"
+              className="flex-1 flex items-start justify-center p-2 text-indigo-500 hover:text-indigo-500"
+            >
+              <div className="text-center w-full">
+                <div className="rounded-full w-full bg-[#FCB52D] text-black text-xs py-1 pl-2 pr-3 leading-none">
+                  <i className="mdi mdi-fire text-base align-middle"></i>{" "}
+                  <span className="align-middle">Member Form</span>
+                </div>
               </div>
-            </div>
-          </a>
-          <a
-            href="/visitor"
-            className="flex-1 flex items-center justify-center p-2 text-gray-500 hover:text-indigo-500"
-          >
-            <div className="text-center">
-              <div className="rounded-full bg-[#FCB52D] text-black text-xs py-1 pl-2 pr-3 leading-none">
-                <i className="mdi mdi-fire text-base align-middle"></i>{" "}
-                <span className="align-middle">Visitor Form</span>
+            </a>
+            <a
+              href="/visitor"
+              className="flex-1 flex items-start justify-center p-2 text-gray-500 hover:text-indigo-500"
+            >
+              <div className="text-center w-full">
+                <div className="rounded-full w-full bg-[#FCB52D] text-black text-xs py-1 pl-2 pr-3 leading-none">
+                  <i className="mdi mdi-fire text-base align-middle"></i>{" "}
+                  <span className="align-middle">Visitor Form</span>
+                </div>
               </div>
-            </div>
-          </a>
-          <a
-            href="/"
-            className="flex-1 flex items-center justify-center p-2 text-gray-500 hover:text-indigo-500"
-          >
-            <div className="text-center">
-              <div className="rounded-full bg-[#FCB52D] text-black text-xs py-1 pl-2 pr-3 leading-none">
-                <i className="mdi mdi-fire text-base align-middle"></i>{" "}
-                <span className="align-middle">Home Page</span>
+            </a>
+            <a
+              href="/"
+              className="flex-1 flex items-start justify-center p-2 text-gray-500 hover:text-indigo-500"
+            >
+              <div className="text-center w-full">
+                <div className="rounded-full w-full bg-[#FCB52D] text-black text-xs py-1 pl-2 pr-3 leading-none">
+                  <i className="mdi mdi-fire text-base align-middle"></i>{" "}
+                  <span className="align-middle">Home Page</span>
+                </div>
               </div>
-            </div>
-          </a>
-        </div>
-        <div className="bg-white w-full text-center py-4">
-          <h1>Dhawan</h1>
+            </a>
+          </div>
         </div>
       </div>
     </>
